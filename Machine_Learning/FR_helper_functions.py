3dbf3034c13943015f8bc8e7959b9657498af308
Fabio Rodrigues <rodriguesfhs@gmail.com>
2025-05-22 02:07:09 +0100

    Update FR_helper_functions.py

--- Machine_Learning/FR_helper_functions.py
+++ Machine_Learning/FR_helper_functions.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 
 # Import metrics libraries
 import torchmetrics
-from torchmetrics import Accuracy
+from torchmetrics import Accuracy, Precision, Recall, F1Score
 from torchmetrics import ConfusionMatrix
 import mlxtend
 from mlxtend.plotting import plot_confusion_matrix
