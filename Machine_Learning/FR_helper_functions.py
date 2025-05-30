4fd0864aa6e82ccc75c873e7df31065220e7e686
Fabio Rodrigues <rodriguesfhs@gmail.com>
2025-05-30 16:02:41 +0100

    Add files via upload

--- Machine_Learning/FR_helper_functions.py
+++ Machine_Learning/FR_helper_functions.py
@@ -23,7 +23,7 @@ from mlxtend.plotting import plot_confusion_matrix
 from mlxtend.evaluate import confusion_matrix
 from torch.utils.tensorboard import SummaryWriter  # Import TensorBoard
 
-from tqdm import tqdm
+from tqdm.notebook import tqdm
 
 
 # In[3]:
